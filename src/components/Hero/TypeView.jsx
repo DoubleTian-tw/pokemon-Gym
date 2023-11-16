fa7b0b5f23444650860ff468adf6facc725c820e
Tian <q135135x@gmail.com>
2023-11-16 15:44:38 +0800

    Add the context of typeView component

--- src/components/Hero/TypeView.jsx
+++ src/components/Hero/TypeView.jsx
@@ -3,6 +3,36 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useHeroContext } from "./useHeroContext";
 import { allType } from "../../data";
 import { useEffect } from "react";
+import { nanoid } from "nanoid";
+const getDamageDetail = (item) => {
+    return item.map(
+        ({ name }) => allType.filter(({ enType }) => name === enType)[0]
+    );
+};
+const DamageDetail = ({ title, detail_item }) => {
+    return (
+        <>
+            <h6>{title}</h6>
+            <div className="mb-2">
+                {detail_item.length === 0
+                    ? "-"
+                    : detail_item.map(({ zhType, bgColor }) => {
+                          return (
+                              <span
+                                  key={nanoid()}
+                                  data-bs-theme="light"
+                                  className="ps-2 pe-2 ms-1 me-1 rounded-3  text-light"
+                                  style={{
+                                      backgroundColor: bgColor,
+                                  }}>
+                                  {zhType}
+                              </span>
+                          );
+                      })}
+            </div>
+        </>
+    );
+};
 const TypeView = () => {
     const {
         showTypeDialog,
@@ -13,10 +43,27 @@ const TypeView = () => {
     let navigate = useNavigate();
     let { type } = useParams();
     let typeFromData = allType.filter((t) => t.enType === type)[0];
-    let zhType = typeFromData.zhType;
-    let bgColor = typeFromData.bgColor;
+    let zhType = typeFromData?.zhType;
+    let bgColor = typeFromData?.bgColor;
     let typeInfo = storeAllTypes.filter((t) => t.name === type)[0];
-    console.log(typeInfo);
+
+    let { damage_relations } = typeInfo || {};
+    let {
+        double_damage_from,
+        double_damage_to,
+        half_damage_from,
+        half_damage_to,
+        no_damage_from,
+        no_damage_to,
+    } = damage_relations || {};
+    let detail_double_damage_from = getDamageDetail(double_damage_from);
+    let detail_double_damage_to = getDamageDetail(double_damage_to);
+    let detail_half_damage_from = getDamageDetail(half_damage_from);
+    let detail_half_damage_to = getDamageDetail(half_damage_to);
+    let detail_no_damage_from = getDamageDetail(no_damage_from);
+    let detail_no_damage_to = getDamageDetail(no_damage_to);
+    // console.log(typeInfo, detail_double_damage_from);
+
     //   let image = getImageById(Number(id));
     //   let buttonRef = React.useRef<HTMLButtonElement>(null);
     function onDismiss() {
@@ -33,11 +80,35 @@ const TypeView = () => {
             id="typeView"
             fullscreen="xs-down">
             <Modal.Header closeButton>
-                <Modal.Title style={{ color: bgColor }}>{zhType}</Modal.Title>
+                <Modal.Title>
+                    <span style={{ color: bgColor }}>{zhType}</span>屬性
+                </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <h2></h2>
-                Woohoo, you are reading this text in a modal!
+                <DamageDetail
+                    title="效果絕佳"
+                    detail_item={detail_double_damage_to}
+                />
+                <DamageDetail
+                    title="效果不好"
+                    detail_item={detail_half_damage_to}
+                />
+                <DamageDetail
+                    title="沒有效果"
+                    detail_item={detail_no_damage_to}
+                />
+                <DamageDetail
+                    title="小心雙倍傷害"
+                    detail_item={detail_double_damage_from}
+                />
+                <DamageDetail
+                    title="小心普通傷害"
+                    detail_item={detail_half_damage_to}
+                />
+                <DamageDetail
+                    title="不會受傷"
+                    detail_item={detail_no_damage_to}
+                />
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={onDismiss}>
@@ -48,39 +119,6 @@ const TypeView = () => {
                 </Button> */}
             </Modal.Footer>
         </Modal>
-        // <Dialog
-        //     aria-labelledby="label"
-        //     onDismiss={onDismiss}
-        //     initialFocusRef={buttonRef}>
-        //     <div
-        //         style={{
-        //             display: "grid",
-        //             justifyContent: "center",
-        //             padding: "8px 8px",
-        //         }}>
-        //         <h1 id="label" style={{ margin: 0 }}>
-        //             {image.title}
-        //         </h1>
-        //         <img
-        //             style={{
-        //                 margin: "16px 0",
-        //                 borderRadius: "8px",
-        //                 width: "100%",
-        //                 height: "auto",
-        //             }}
-        //             width={400}
-        //             height={400}
-        //             src={image.src}
-        //             alt=""
-        //         />
-        //         <button
-        //             style={{ display: "block" }}
-        //             ref={buttonRef}
-        //             onClick={onDismiss}>
-        //             Close
-        //         </button>
-        //     </div>
-        // </Dialog>
     );
 };
 export default TypeView;
