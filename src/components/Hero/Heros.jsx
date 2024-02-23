eeb323230d731558b6f452896ebf0808ac48ffba
Tian <q135135x@gmail.com>
2024-02-23 20:57:29 +0800

    Move the child component to the same level as the parent

--- src/components/Hero/Heros.jsx
+++ src/components/Hero/Heros.jsx
@@ -8,10 +8,44 @@ import {
     useFetchTierPokemon,
     postFirebase_whenClose,
 } from "./useFetchFirebase";
-import { Suspense, lazy, useEffect, useMemo, useState } from "react";
+import { Suspense, lazy, useEffect } from "react";
 import Loading from "../Loading";
 
 const CharacterGroupsLoading = lazy(() => import("./CharacterGroups.jsx"));
+
+const CheckBestDamage = () => {
+    const {
+        selectImg,
+        showInfo_bestDamage,
+        showType_bestDamage,
+        bestDamage,
+        tierPokemon,
+    } = useHeroContext();
+
+    if (selectImg.length === 0) return;
+    if (bestDamage.length === 0)
+        return <p className="best-damage-no-data">沒有共同相剋的屬性!</p>;
+
+    if (Object.keys(tierPokemon).length > 0) {
+        return (
+            <TierGroups
+                showInfo_select={showInfo_bestDamage.type}
+                showType_select={showType_bestDamage.type}
+                displayTier={tierPokemon}
+                bestDamage={bestDamage}
+            />
+        );
+    } else {
+        return (
+            <>
+                <p></p>
+                <p className="best-damage-no-data">
+                    目前沒有推薦屬性的神奇寶貝
+                </p>
+            </>
+        );
+    }
+};
 
 const Heros = () => {
     const {
@@ -51,31 +85,7 @@ const Heros = () => {
             window.removeEventListener("beforeunload", handleTabClose);
         };
     }, [clickImg]);
-    const CheckBestDamage = () => {
-        if (selectImg.length === 0) return;
-        if (bestDamage.length === 0)
-            return <p className="best-damage-no-data">沒有共同相剋的屬性!</p>;
 
-        if (Object.keys(tierPokemon).length > 0) {
-            return (
-                <TierGroups
-                    showInfo_select={showInfo_bestDamage.type}
-                    showType_select={showType_bestDamage.type}
-                    displayTier={tierPokemon}
-                    bestDamage={bestDamage}
-                />
-            );
-        } else {
-            return (
-                <>
-                    <p></p>
-                    <p className="best-damage-no-data">
-                        目前沒有推薦屬性的神奇寶貝
-                    </p>
-                </>
-            );
-        }
-    };
     return (
         <main>
             <section className="hero">
