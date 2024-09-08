b43f1c5e0737d0efdc20de05c3cfd1555fcc4901
Paddy Tian <q135135x@gmail.com>
2024-09-08 22:03:09 +0800

    feat/Add config of alias

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from 'url';
 
-// https://vitejs.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
     plugins: [react()],
     base: process.env.VITE_BASE_PATH || "/pokemon-Gym",
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "./src"),
+        },
+    },
 });
