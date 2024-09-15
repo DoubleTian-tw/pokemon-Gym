07ab5dde52568d7938e50a2d1657d90cfab3e434
Paddy Tian <q135135x@gmail.com>
2024-09-15 14:43:21 +0800

    feat/Add shadcn and tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
-import { resolve, dirname } from "path";
-import { fileURLToPath } from 'url';
+import { defineConfig } from "vite";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
     plugins: [react()],
     base: process.env.VITE_BASE_PATH || "/pokemon-Gym",
