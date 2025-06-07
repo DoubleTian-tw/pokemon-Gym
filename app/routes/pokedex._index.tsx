4b77ff250cd7b1278b6a4eff0e33d836553a0e35
Paddy Tian <q135135x@gmail.com>
2025-06-07 16:01:01 +0800

    feat: Add basic route's pages

---
+++ app/routes/pokedex._index.tsx
@@ -0,0 +1,5 @@
+const Pokedex = () => {
+    return <div>Pokedex</div>;
+};
+
+export default Pokedex;
---
+++ app/routes/pokemon-Gym._index.tsx
@@ -0,0 +1,5 @@
+const PokemonGym = () => {
+    return <div>PokemonGym</div>;
+};
+
+export default PokemonGym;
